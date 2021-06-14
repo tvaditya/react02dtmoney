300e00c143100cbdf9e207730c666cb9b30fc753
tvaditya <tvaditya@gmail.com>
2021-06-14 17:16:58 -0300

    Global styled components created

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
+import { GlobalStyle} from "./styles/global";
+
 export function App() {
   return (
     <div className="App">
       <h1>Hello DtMoney</h1>
+        <GlobalStyle />
     </div>
   );
 }
 
-// export default App;
