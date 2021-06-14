c442ebefc9d155d975ea33bee26f58f98c75e80f
tvaditya <tvaditya@gmail.com>
2021-06-14 18:15:15 -0300

    Header component created

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import { GlobalStyle} from "./styles/global";
+import {Header} from "./components/Header";
 
 export function App() {
   return (
-    <div className="App">
-      <h1>Hello DtMoney</h1>
-        <GlobalStyle />
-    </div>
+    <>
+      <Header />
+      <GlobalStyle />
+    </>
   );
 }
 
