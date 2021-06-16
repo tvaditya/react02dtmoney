c77e68996125281a9b1f5f671fb49dfd97fae74b
tvaditya <tvaditya@gmail.com>
2021-06-16 01:34:06 -0300

    Transaction Context basic created

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import {Dashboard} from "./components/Dashboard";
 import Modal from "react-modal";
 import {useState} from "react";
 import {NewTransactionModal} from "./components/NewTransactionModal";
+import {TransactionsProvider} from "./TransactionsContext";
 
 Modal.setAppElement('#root');
 
@@ -19,12 +20,14 @@ export function App() {
     }
 
     return (
-    <>
+    <TransactionsProvider>
         <Header onOpenNewTransactionModal={handleOpenNewTransactionModal}/>
         <Dashboard/>
-        <NewTransactionModal isOpen={isNewTransactionModalOpen} onRequestClose={handleCloseNewTransactionModel} />
+        <NewTransactionModal
+            isOpen={isNewTransactionModalOpen}
+            onRequestClose={handleCloseNewTransactionModel} />
         <GlobalStyle />
-    </>
+    </TransactionsProvider>
   );
 }
 
